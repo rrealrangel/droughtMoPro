8fd232d0eb9f2d89673c6b1766525c4721ed0cac
Roberto Real-Rangel <rrealr@iingen.unam.mx>
2019-07-20 20:32:41 -0700

    Magnitude maps were included

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ import mosemm_products.nc4_to_kml as tokml
 
 settings = dmgr.Configurations(config_file='config.toml')
 
-# Export drought intensity maps (KML).
+# Export drought intensity maps to KML format files.
 if settings.intensity_maps['export']:
-    print("- Exporting drought intensity map(s).")
+    print("- Exporting drought intensity maps.")
 
     # Available source data to generate drought monitoring products.
     input_files = dmgr.list_files(
@@ -92,32 +92,84 @@ if settings.intensity_maps['export']:
             message="- Exporting the drought intensity maps",
             units='maps'
             )
-#
-#
-#
-#
-#
-#
-#
-#
-#input_files = maps_to_export(input_dir=config['input_dir'])
-#home_dir = 'C:/Users/rreal/OneDrive/datasets/conagua_mosemm/v2/maps'
-#set_grass_env(
-#        reference_array=xr.open_dataset(input_files[0]).Drought_intensity)
-#    
-#
-#
+
+# Export drought magnitude maps to KML format files.
+if settings.magnitude_maps['export']:
+    print("- Exporting drought magnitude maps.")
+
+    # Available source data to generate drought monitoring products.
+    input_files = dmgr.list_files(
+        parent_dir=settings.magnitude_maps['input_dir'],
+        pattern='**/*.nc4'
+        )
+
+    if settings.general['period_to_export'] == 'last':
+        available_periods = sorted(list(set([
+            i.stem.split('_')[-1] for i in input_files
+            ])))
+        files_to_export = sorted([
+            i
+            for i in input_files
+            if ((available_periods[-1] in str(i)) and '-01_' in str(i))
+            ])
+
+    elif settings.general['period_to_export'] == 'all':
+        files_to_export = sorted([i for i in input_files if '-01_' in str(i)])
+
+    elif isinstance(settings.general['period_to_export'], list):
+        first = settings.general['period_to_export'][0]
+        first = dt.date(
+            year=int(first[:4]),
+            month=int(first[-2:]),
+            day=1
+            )
+        last = settings.general['period_to_export'][-1]
+        last = dt.date(
+            year=int(last[:4]),
+            month=int(last[-2:]),
+            day=1
+            ) + DateOffset(months=1)
+        dates = date_range(start=first, end=last, freq='1M')
+        datestamps_to_export = [
+            str(i.year) + str(i.month).zfill(2)
+            for i in dates
+            ]
+        files_to_export = sorted([
+            i
+            for i in input_files
+            for j in datestamps_to_export
+            if ((j in str(i)) and ('-01_' in str(i)))
+            ])
+
+    elif isinstance(settings.general['period_to_export'], unicode):
+        files_to_export = sorted([
+            i
+            for i in input_files
+            if ((settings.general['period_to_export'] in str(i)) and
+                ('-01_' in str(i)))
+            ])
+
+    else:
+        print("- Nothing to export.")
+
+    for f, file_to_export in enumerate(files_to_export):
+        tokml.export_kml_dmag(
+            input_file=file_to_export,
+            output_dir=settings.magnitude_maps['output_dir'],
+            output_fname_prefix=settings.general['output_prefix'],
+            overwrite=settings.general['overwrite']
+            )
+        dmgr.progress_message(
+            current=(f + 1),
+            total=len(files_to_export),
+            message="- Exporting the drought magnitude maps",
+            units='maps'
+            )
+
+# Export time series.
 
 
 
-
-
-
-
-
-## Compute and export drought magnitude maps (KML).
-#
-## Export time series.
 #
 ## Export reports (csv; or xlsx?).
 #if settings.reports['compute']:
--- mosemm_products/nc4_to_kml.py
+++ mosemm_products/nc4_to_kml.py
@@ -13,15 +13,6 @@ import numpy as np
 import xarray as xr
 
 
-#def maps_to_export(input_dir):
-#    input_files = []
-#
-#    for pattern in ['**/*201904.nc', '**/*201904.nc4']:
-#        input_files.extend(Path(input_dir).glob(pattern))
-#
-#    return(input_files)
-
-
 def rename_indices(old_name):
     names = {
         'SPI': 'SPI',
@@ -29,47 +20,41 @@ def rename_indices(old_name):
         'SSI': 'SSI',
         'MSDI_PRERUN': 'PreRun',
         'MSDI_PRESMO': 'PreSMo',
-        'MSDI_PRESMORUN': 'PreSMoRun'}
+        'MSDI_PRESMORUN': 'PreSMoRun'
+        }
     return(names[old_name])
 
 
-#def round_mult(num, mult, to):
-#    if to == 'up':
-#        return(int(mult * round(float(num + mult) / mult)))
-#
-#    elif to == 'down':
-#        return(int(mult * round(float(num - mult) / mult)))
-
-
-def set_grass_env(reference_array):
+def set_grass_env(reference):
     grass.run_command('g.gisenv', set="DEBUG=0")
     grass.run_command('g.gisenv', set="GRASS_VERBOSE=-1")
-#    os.environ['GRASS_VERBOSE'] = '-1'
-    # os.environ['DEBUG'] = '0'
-    res = reference_array.lat.diff(dim='lat').values[0]
-    n = max(reference_array.lat).values + res
-    s = min(reference_array.lat).values - res
-    e = max(reference_array.lon).values + res
-    w = min(reference_array.lon).values - res
+    res = reference.lat.diff(dim='lat').values[0]
+    n = max(reference.lat).values + res
+    s = min(reference.lat).values - res
+    e = max(reference.lon).values + res
+    w = min(reference.lon).values - res
     grass.run_command(
         'g.region',
         n=n,
         s=s,
         e=e,
         w=w,
-        rows=len(reference_array.lat),
-        cols=len(reference_array.lon),
+        rows=len(reference.lat),
+        cols=len(reference.lon),
         res=res,
         overwrite=True,
         quiet=True
         )
 
 
-def export_to_grass(data_array, map_name, nodata):
+def export_to_grass(data, map_name, nodata):
     # Export the array as a raster map to GRASS-GIS
     output_map = garray.array()
-    output_map[:, :] = np.flipud(data_array)
-    output_map.write(mapname=map_name, overwrite=True)
+    output_map[:, :] = np.flipud(data)
+    output_map.write(
+        mapname=map_name,
+        overwrite=True
+        )
 
 
 def raster2vector_dint(input_map):
@@ -89,7 +74,8 @@ def raster2vector_dint(input_map):
             input=input_map,
             output=input_map + '_reclass',
             overwrite=True,
-            quiet=True)
+            quiet=True
+            )
     grass.run_command(
             'r.to.vect',
             flags='sv',
@@ -97,52 +83,57 @@ def raster2vector_dint(input_map):
             output=input_map,
             type='area',
             overwrite=True,
-            quiet=True)
+            quiet=True
+            )
 
     # Update the label (W4, W3, W2, etc.) of the categories used
     labels_cond = {
-            'cat=1': 'D4',
-            'cat=2': 'D3',
-            'cat=3': 'D2',
-            'cat=4': 'D1',
-            'cat=5': 'D0',
-            'cat=6': 'Normal',
-            'cat=7': 'W0',
-            'cat=8': 'W1',
-            'cat=9': 'W2',
-            'cat=10': 'W3',
-            'cat=11': 'W4'}
+        'cat=1': 'D4',
+        'cat=2': 'D3',
+        'cat=3': 'D2',
+        'cat=4': 'D1',
+        'cat=5': 'D0',
+        'cat=6': 'Normal',
+        'cat=7': 'W0',
+        'cat=8': 'W1',
+        'cat=9': 'W2',
+        'cat=10': 'W3',
+        'cat=11': 'W4'
+        }
     main_dir = os.path.dirname(os.path.abspath('__file__'))
-#    main_dir = main_dir.replace("\\", "/")
 
     with open(main_dir + "/update_db.sql", "w") as text_file:
         text_file.write(
-                'ALTER TABLE {} ADD COLUMN rgb varchar(11);\n'.format(
-                        input_map))
+            'ALTER TABLE {} ADD COLUMN rgb varchar(11);\n'.format(input_map)
+            )
 
         for key, val in labels_cond.iteritems():
             text_file.write(
-                    "UPDATE {} SET label='{}' WHERE {};\n".format(
-                            input_map, val, key))
+                "UPDATE {} SET label='{}' WHERE {};\n".format(
+                        input_map, val, key
+                        )
+                )
 
     # Add the color rules for the drought vector map
     colors_cond = {
-            'cat=1': '189:0:38',
-            'cat=2': '240:59:32',
-            'cat=3': '253:141:60',
-            'cat=4': '254:204:92',
-            'cat=5': '255:255:178',
-            'cat=6': '255:255:255',
-            'cat=7': '240:249:232',
-            'cat=8': '186:228:188',
-            'cat=9': '123:204:196',
-            'cat=10': '67:162:202',
-            'cat=11': '8:104:172'}
+        'cat=1': '189:0:38',
+        'cat=2': '240:59:32',
+        'cat=3': '253:141:60',
+        'cat=4': '254:204:92',
+        'cat=5': '255:255:178',
+        'cat=6': '255:255:255',
+        'cat=7': '240:249:232',
+        'cat=8': '186:228:188',
+        'cat=9': '123:204:196',
+        'cat=10': '67:162:202',
+        'cat=11': '8:104:172'
+        }
 
     with open(main_dir + "/update_db.sql", "a") as text_file:
         for key, val in colors_cond.iteritems():
             text_file.write("UPDATE {} SET rgb='{}' WHERE {};\n".format(
-                    input_map, val, key))
+                input_map, val, key
+                ))
 
     grass.run_command('db.execute', input=main_dir+"/update_db.sql")
 
@@ -159,7 +150,8 @@ def raster2vector_dmag(input_map):
             input=input_map,
             output=input_map + '_reclass',
             overwrite=True,
-            quiet=True)
+            quiet=True
+            )
     grass.run_command(
             'r.to.vect',
             flags='sv',
@@ -167,7 +159,8 @@ def raster2vector_dmag(input_map):
             output=input_map,
             type='area',
             overwrite=True,
-            quiet=True)
+            quiet=True
+            )
 
     # Update the label (W4, W3, W2, etc.) of the categories used
     labels_cond = {
@@ -176,19 +169,21 @@ def raster2vector_dmag(input_map):
         'cat=2': 'M2',
         'cat=3': 'M3',
         'cat=4': 'M4',
-        'cat=5': 'M5'}
+        'cat=5': 'M5'
+        }
     main_dir = os.path.dirname(os.path.abspath('__file__'))
-#    main_dir = main_dir.replace("\\", "/")
 
     with open(main_dir + "/update_db.sql", "w") as text_file:
         text_file.write(
-                'ALTER TABLE {} ADD COLUMN rgb varchar(11);\n'.format(
-                        input_map))
+            'ALTER TABLE {} ADD COLUMN rgb varchar(11);\n'.format(
+                input_map
+                ))
 
         for key, val in labels_cond.iteritems():
             text_file.write(
-                    "UPDATE {} SET label='{}' WHERE {};\n".format(
-                            input_map, val, key))
+                "UPDATE {} SET label='{}' WHERE {};\n".format(
+                    input_map, val, key
+                    ))
 
     # Add the color rules for the drought vector map
     colors_cond = {
@@ -197,18 +192,22 @@ def raster2vector_dmag(input_map):
         'cat=2': '253:140:0',
         'cat=3': '254:0:0',
         'cat=4': '128:0:128',
-        'cat=5': '64:0:64'}
+        'cat=5': '64:0:64'
+        }
 
     with open(main_dir + "/update_db.sql", "a") as text_file:
         for key, val in colors_cond.iteritems():
-            text_file.write("UPDATE {} SET rgb='{}' WHERE {};\n".format(
-                    input_map, val, key))
+            text_file.write(
+                "UPDATE {} SET rgb='{}' WHERE {};\n".format(
+                    input_map, val, key
+                    )
+                )
 
     grass.run_command('db.execute', input=main_dir+"/update_db.sql")
 
 
-def kml_style_dint(file_name):
-    with open(file_name, 'r') as f:
+def kml_style_dint(input_file):
+    with open(input_file, 'r') as f:
         kml = f.read()
 
     kml = kml.splitlines()
@@ -225,8 +224,10 @@ def kml_style_dint(file_name):
             pmark_end.append(ln)
 
     def style(hexcolor):
-        style = ("\t<Style><PolyStyle><color>" + hexcolor +
-                 "</color><outline>0</outline></PolyStyle></Style>")
+        style = (
+            "\t<Style><PolyStyle><color>" + hexcolor +
+            "</color><outline>0</outline></PolyStyle></Style>"
+            )
 
         return(style)
 
@@ -290,14 +291,14 @@ def kml_style_dint(file_name):
                         if "<Style>" in kml[line2]:
                             kml[line2] = style('99ac6808')
 
-    with open(file_name, 'w') as kml_out:
+    with open(input_file, 'w') as kml_out:
         for ln, line in enumerate(kml):
             kml_out.write(line)
             kml_out.write("\n")
 
 
-def kml_style_dmag(file_name):
-    with open(file_name, 'r') as f:
+def kml_style_dmag(input_file):
+    with open(input_file, 'r') as f:
         kml = f.read()
 
     kml = kml.splitlines()
@@ -314,8 +315,10 @@ def kml_style_dmag(file_name):
             pmark_end.append(ln)
 
     def style(hexcolor):
-        style = ("\t<Style><PolyStyle><color>" + hexcolor +
-                 "</color><outline>0</outline></PolyStyle></Style>")
+        style = (
+            "\t<Style><PolyStyle><color>" + hexcolor +
+            "</color><outline>0</outline></PolyStyle></Style>"
+            )
 
         return(style)
 
@@ -352,32 +355,24 @@ def kml_style_dmag(file_name):
                         if "<Style>" in kml[line2]:
                             kml[line2] = style('99400040')
 
-    with open(file_name, 'w') as kml_out:
+    with open(input_file, 'w') as kml_out:
         for ln, line in enumerate(kml):
             kml_out.write(line)
             kml_out.write("\n")
 
 
-def export_kml_dmag(input_map, output_file):
-    grass.run_command(
-        'v.out.ogr',
-        input=input_map,
-        output=output_file,
-        format='KML',
-        overwrite=True,
-        quiet=True)
-    kml_style_dmag(file_name=output_file)
-
-
 def export_kml_dint(
-        input_file, output_dir, output_fname_prefix='', overwrite=False
+        input_file, output_dir, nodata=-32768, output_fname_prefix='',
+        overwrite=False
         ):
+    """
+    """
     # Open the data.
     data = xr.open_dataset(input_file).Drought_intensity
 
     # Vectorize the input data in GRASS GIS.
     set_grass_env(data)
-    export_to_grass(data, map_name='map_to_export', nodata=-32768)
+    export_to_grass(data, map_name='map_to_export', nodata=nodata)
     raster2vector_dint(input_map='map_to_export')
 
     # Export the data as a KML file.
@@ -409,27 +404,52 @@ def export_kml_dint(
         quiet=True
         )
     kml_style_dint(
-        file_name=str(output_full_path)
+        input_file=str(output_full_path)
         )
 
 
-## Export drought magnitude maps
-#for i, input_file in enumerate(input_files):
-#    # TODO: import all datasets simultaneously using xarray.open_mfdataset.
-#    data_array = xr.open_dataset(input_file).Drought_magnitude
-#    export_to_grass(data_array, map_name='map_to_export', nodata=-32768)
-#    raster2vector_dmag(input_map='map_to_export')
-#    output_dir = Path(
-#        home_dir + '/' + data_array.attrs['DroughtIndex'].lower()
-#        + data_array.attrs['TemporalScale'][:2].strip().zfill(2) + '/'
-#        + str(data_array.time.dt.year.values))
-#    output_dir.mkdir(parents=True, exist_ok=True)
-#    output_file = (
-#        'IIUNAM_MERRA2_' + rename_indices(data_array.attrs['DroughtIndex'])
-#        + data_array.attrs['TemporalScale'][:2].strip().zfill(2) + '_dm_'
-#        + str(data_array.time.dt.year.values)
-#        + str(data_array.time.dt.month.values).zfill(2) + '_res.kml')
-#    export_kml_dmag(
-#        input_map='map_to_export',
-#        output_file=str(output_dir / output_file))
-#    progress_bar(current=i + 1, total=len(input_files), message="- Processing")
+def export_kml_dmag(
+        input_file, output_dir, nodata=-32768, output_fname_prefix='',
+        overwrite=False
+        ):
+    """
+    """
+    # Open the data.
+    data = xr.open_dataset(input_file).Drought_magnitude
+
+    # Vectorize the input data in GRASS GIS.
+    set_grass_env(data)
+    export_to_grass(data, map_name='map_to_export', nodata=nodata)
+    raster2vector_dmag(input_map='map_to_export')
+
+    # Export the data as a KML file.
+    # - Build the output file name.
+    index = data.attrs['DroughtIndex'].lower()
+    time_scale = data.attrs['TemporalScale'][:2].strip().zfill(2)
+    year = str(data.time.dt.year.values)
+    output_subdir = Path(index + time_scale) / year
+    output_fname = (
+        output_fname_prefix + rename_indices(data.attrs['DroughtIndex'])
+        + time_scale + '_dm_' + year +
+        str(data.time.dt.month.values).zfill(2) + '_res.kml'
+        )
+
+    # - Make the directory, in case it does not exists.
+    (Path(output_dir) / output_subdir).mkdir(
+        parents=True,
+        exist_ok=True
+        )
+
+    # Export the KML file and modify its style.
+    output_full_path = Path(output_dir) / output_subdir / output_fname
+    grass.run_command(
+        'v.out.ogr',
+        input='map_to_export',
+        output=str(output_full_path),
+        format='KML',
+        overwrite=True,
+        quiet=True
+        )
+    kml_style_dmag(
+        input_file=str(output_full_path)
+        )

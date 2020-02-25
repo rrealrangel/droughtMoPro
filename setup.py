77b65566fbdc80dc607a4c02a15081531ceb6e64
Roberto Real-Rangel <rrealr@iingen.unam.mx>
2020-02-25 00:25:37 -0800

    Setup file updated

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from setuptools import setup
 setup(
      name='droughtMoPro',
      version='v0.1.3.2',
-     description='Generator of drought monitoring products.',
-     url='https://github.com/rrealrangel/droughtMoPro',
      author='R. A. Real-Rangel',
      author_email='rrealr@iingen.unam.mx',
+     description='Generator of drought monitoring products.',
      license='GPL-3.0',
-     packages=[
-         'droughtMoPro'
-         ],
+     keywords="drought monitoring mapping series",
+     url='https://github.com/rrealrangel/droughtMoPro',
+     packages=['droughtMoPro'],
+     long_description=read('README'),
+     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Hydrology",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"]
      install_requires=[
          'datetime',
          'dask',
